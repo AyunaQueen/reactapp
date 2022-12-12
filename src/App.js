37ed227b00c36b8c4e0dd0d98b545487e8af998f
Richelle Kim <ayunawinliens@gmail.com>
2022-12-11 22:22:46 -0300

    my firt commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+
+
+export default function App(){
+  return(
+    <>
+      <h1>Olá Mundo</h1>
+      <p>Teste para sites em react</p>
+    </>
+  )
+}
+
